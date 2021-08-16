85449e2814a0477f771a894980616d7430d08a6e
matheuscarv69 <matheus9126@gmail.com>
2021-08-16 17:17:06 -0400

    :sparkles: feat: queries consulting db

--- src/main/java/compras/QueryGraphQL.java
+++ src/main/java/compras/QueryGraphQL.java
@@ -1,17 +1,36 @@
 package compras;
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import compras.entities.client.model.Client;
+import compras.entities.client.repository.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class QueryGraphQL implements GraphQLQueryResolver {
+
+    @Autowired
+    private ClientRepository clientRepository;
 
     public String hello() {
         return "Hello World";
     }
 
-    public Integer soma(Integer a, Integer b) {
+    public Integer sum(Integer a, Integer b) {
         return a + b;
     }
 
+    public Client getClient(Long id) {
+        Client client = clientRepository
+                .findById(id)
+                .orElseThrow(() -> new RuntimeException("Client not found"));
+
+        return client;
+    }
+
+    public List<Client> getAllClients() {
+        return clientRepository.findAll();
+    }
 }
---
+++ src/main/java/compras/entities/client/model/Client.java
@@ -0,0 +1,43 @@
+package compras.entities.client.model;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public String name;
+    public String email;
+
+    public Client(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public Client() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
